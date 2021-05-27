d922d8220a6038662b670e2e1682aec1655dd093
Daniel Mendez <danielsaso@gmail.com>
2021-05-27 14:02:11 +0200

    Prepare rails app with external specs

---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+
+
+  validates :movie_id, uniqueness: { scope: :list_id,
+    message: "this movie already exists in this list" }
+
+  # validates_uniqueness_of :movie_id, :scope => [:list_id]
+end
