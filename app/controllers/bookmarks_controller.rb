225640471c64ec19fcfec408ba1b9755a2cbaf00
Daniel Mendez <danielsaso@gmail.com>
2021-05-28 12:39:31 +0200

    added root to routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,7 +16,7 @@ class BookmarksController < ApplicationController
     @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to root_path(@list)
     else
       render :new
     end
